03c384eb9ff5b3c4480642bafa453f2cc96e8185
Adrian42X <89139357+Adrian42X@users.noreply.github.com>
2023-05-12 17:17:12 +0300

    User page created

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,3 @@
-import { HttpBackend, HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,23 +9,18 @@ export class HomeComponent implements OnInit{
   
   registerMode=false;
   users:any;
-  url="https://localhost:7024/Users";
-  constructor(private http:HttpClient){}
+  
+  constructor(){}
+
   ngOnInit(): void {
-    this.getUsers();
+    
   }
 
   registerToggle(){
     this.registerMode=!this.registerMode;
   }
 
-  getUsers(){
-    this.http.get(this.url).subscribe({
-      next:response=>this.users=response,
-      error:error=>console.log(error),
-      complete:()=>console.log('Request has completed')
-    })
-  }
+  
 
   cancelRegisterMode(event: boolean){
     this.registerMode=event;
